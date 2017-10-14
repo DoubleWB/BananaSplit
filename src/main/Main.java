92e4bb918eeec26c74c88aa518ac89cf130cef4e
Will Becker <dustfingerss@gmail.com>
2017-10-14 06:00:48 -0400

    some work on main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,18 @@
 package main;
 
 import java.util.ArrayList;
+import resources.*;
 
 public class Main {
 	
+	private static ArrayList<Person> people = new ArrayList<Person>();
+	private static ArrayList<Item> outstandingItems = new ArrayList<Item>();
+	
+	public void on_Finished(Item finished) {
+		people.get(0).split(finished, 1, this.people);
+	}
+		
 	public static void main(String[] args) {
-		ArrayList<resources.Person> people = new ArrayList<resources.Person>();
-		ArrayList<resources.Item> outstandingItems = new ArrayList<resources.Item>();
 		
 		System.out.println(people.size());
 		
