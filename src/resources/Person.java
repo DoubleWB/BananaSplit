0086033a45059019e04b4d6c5bbc16f3a51dbf25
leekev1 <lee.kev@husky.neu.edu>
2017-10-14 05:44:15 -0400

    adding backbone code

--- src/resources/Person.java
+++ src/resources/Person.java
@@ -1,5 +1,18 @@
 package resources;
 
 public class Person {
-
+	
+	private int pId;
+	private String name;
+	private double debt;
+	
+	Person(int pId, String name, double debt) {
+		this.pId = pId;
+		this.name = name;
+		this.debt = debt;
+	}
+	
+	Person(int pId, String name) {
+		this(pId, name, 0);
+	}
 }
